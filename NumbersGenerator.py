e24bbcee882c2c62b9d7d62faec8d3faabae7017
kuznecov <kuznecov@protei.ru>
2024-12-05 00:16:36 +0300

    CMake lib fixed.

--- NumbersGenerator.py
+++ NumbersGenerator.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-random_numbers = np.random.randint(-99999, 99999, size=1000000, dtype=np.int32)
+random_numbers = np.random.randint(-99999, 99999, size=100000, dtype=np.int32)
 
 with open("build/bin/InputTape", "w") as f:
     for number in random_numbers:
